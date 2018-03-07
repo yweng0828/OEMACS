01dece6caf123b9e26b382f6c6ef050d6e4484a0
Budding0828 <569895841@qq.com>
2018-03-07 21:48:29 +0800

    first commit

---
+++ OEMACS/test.cpp
@@ -0,0 +1,7 @@
+#include "test.h"
+#include "stdafx.h"
+#include "targetver.h"
+
+void Test1() {
+	
+}
---
+++ OEMACS/test.h
@@ -0,0 +1,15 @@
+#pragma once
+//Ä£Äâ²âÊÔ
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <cmath>
+
+void Test1();
+void Test2();
+void Test3();
+void Test4();
+void Test5();
